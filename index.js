71585ba2aa3b3187410fa12dcd7cc0f17ce9e772
nsalimova <nsalimova@oggettoweb.com>
2024-10-09 09:14:47 +0300

    Basics. Test tasks. Fix after review

--- index.js
+++ index.js
@@ -6,16 +6,43 @@
 // Не поняла нужно вернуть дату через объект Date или просто сформировать строку.
 // Сделала оба варианта
 
+// Добавила соответствующие проверки. Изначально не придала этим проверкам никакого значения, так как подумала, что
+// главное показать, что умею работать с функциями. А уточнения, в каком формате должны возвращаться данные, не было.
+
 function formatDate (year, mounth, day) {
+    if (day === 0 || day > 31) {
+        alert('Введите другое значение дня. Значение должно быть больше 0 и меньше 32');
+        return;
+    }
+    if (String(year).length < 4) {
+        alert('Значение года должно состоять из 4-х символов');
+        return;
+    }
+
+    if (mounth === 0 || mounth > 12) {
+        alert('Введите другое значение месяца. Значение должно быть больше 0 и меньше 13');
+        return;
+    }
+
     const dd = day >= 10 ? day : '0' + day;
     const mm = mounth >= 10 ? mounth : '0' + mounth;
 
-    return `${year}-${mm}-${dd}`
+    return `${dd} + '.' + ${mm} + '.' + ${year}`;
 
-    //return new Date(year, mounth, day)
+    // Через объект new Date
+
+    // const date = new Date(year, mounth, day);
+    //
+    // let dd = date.getDate();
+    // if (dd < 10) dd = '0' + dd;
+    //
+    // let mm = date.getMonth() + 1;
+    // if (mm < 10) mm = '0' + mm;
+    //
+    // let yy = date.getFullYear();
+    //
+    // return dd + '.' + mm + '.' + yy;
 }
-
-console.log(formatDate(7, 10, 12));
 
 // Map и Set
 
@@ -25,7 +52,8 @@ function removeDuplicateValues(array) {
 
 console.log(removeDuplicateValues(["apple", "banana", "carrot", "apple", "juice", "sugar", "banana", "tomato", "bread"]));
 
-// Обънкты
+// Объекты
+// Упустила момент, что один из ключей не является примитивом. Поправила
 
 let Data = {
     name: "John",
@@ -35,16 +63,20 @@ let Data = {
     city: "London",
     adddress: "Oxford street, 25",
     ZIP: 132456,
-    hobby: ["reading books", "jogging", "play guitar"]
+    hobby: ["reading books", "jogging", "play guitar", "tets"]
 }
 
-let copyData = {};
+function deepCopy(property) {
+    if (typeof property !== 'object') return property;
 
-for (key in Data) {
-    copyData[key] = Data[key];
+    let copyData = Array.isArray(property) ? [] : {};
+
+    for (let key in property) {
+        copyData[key] = deepCopy(property[key]);
+    }
+
+    return copyData;
 }
-
-console.log(copyData)
 
 // Деструктуризация
 
